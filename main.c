53927f7db7d11f28162aedf7fbde35523907de91
Mamba1990 <h.daoudim@outlook.com>
2023-05-29 07:39:11 -0700

    damn

--- main.c
+++ main.c
@@ -12,21 +12,16 @@ int main(void)
 	int len;
 	int len2;
 
-	len = _printf("Let's try to printf a simple sentence.\n");
+        len = _printf("Let's try to printf a simple sentence.\n");
 	printf("Number of characters printed: %d\n", len);
 	len2 = printf("Let's try to printf a simple sentence.\n");
 	printf("Number of characters printed: %d\n", len2);
 	len = _printf("Character:[%c]\n", 'H');
 	printf("Number of characters printed: %d\n", len);
-	printf("Character:[%c]\n", 'H');
-	len = _printf("String:[%s]\n", "I am a string !");
-	printf("Number of characters printed: %d\n", len);
-	printf("String:[%s]\n", "I am a string !");
+	len2 = printf("Character:[%c]\n", 'H');
 	len = _printf("Percent:[%%]\n");
 	printf("Number of characters printed: %d\n", len);
 	len2 = printf("Percent:[%%]\n");
 	printf("Number of characters printed: %d\n", len2);
-	_printf("Lengith:[%d, %i]\n", len, len);
-	printf("Length:[%d, %i]\n", len2, len2);
 return (0);
 }
