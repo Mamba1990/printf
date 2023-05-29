8b121bbb7a085737b9cc7f5c52ad0a0c5d4b1909
Azizahabb <habchaneaziza@gmail.com>
2023-05-29 09:53:18 -0700

    main c

--- main.c
+++ main.c
@@ -9,8 +9,8 @@
  */
 int main(void)
 {
-	int length = _printf("String:[%s] character:[%c]\n", "I am a string ", 'H');
+	int len = _printf("String:[%s] character:[%c]\n", "I am a string ", 'H');
 
-	printf("Number of characters printed: %d\n", length);
+	printf("Number of characters printed: %d\n", len);
 return (0);
 }
