0f0831995c8953f3683fd932517999ceb0bec5f5
Mamba1990 <h.daoudim@outlook.com>
2023-05-29 05:56:18 -0700

    0

--- main.c
+++ main.c
@@ -12,10 +12,10 @@ int main(void)
 	int len;
 	int len2;
 
-	len = _printf("Let's try to printf a simple sentence.\n");
+/*	len = _printf("Let's try to printf a simple sentence.\n");
 	printf("Number of characters printed: %d\n", len);
 	len2 = printf("Let's try to printf a simple sentence.\n");
-	printf("Number of characters printed: %d\n", len2);
+	printf("Number of characters printed: %d\n", len2);*/
 	len = _printf("Character:[%c]\n", 'H');
 	printf("Number of characters printed: %d\n", len);
 	printf("Character:[%c]\n", 'H');
